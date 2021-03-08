7ab32d7d03e33328f02b0df48c97befba7f750ed
Tyler Bartlett <tbartlett17@mail.wou.edu>
2021-03-07 22:15:38 -0800

    implemented the admin page to run scrapers

---
+++ SpillTracker/SpillTracker/Models/AdminVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpillTracker.Models
+{
+    public class AdminVM
+    {
+        public IEnumerable<Chemical> chemicals { get; set; }
+
+        public IEnumerable<StatusTime> times { get; set; }
+
+    }
+}
---
+++ SpillTracker/SpillTracker/wwwroot/js/AdminPanel.js
@@ -0,0 +1,52 @@
+﻿
+
+$("#EPCRAscrapeBtn").click(function (e) {
+    var inputXpath = $("#EPCRAtableXpath").val();
+    console.log("running EPCRA Scrapper with xpath: " + inputXpath);
+    $("#EPCRAoverlay").css({ display: "flex" });
+
+    $.ajax({
+        type: "POST",
+        async: false,
+        data: { xpath: inputXpath },
+        url: "/Admin/ScrapeEPCRAtable",
+        success: successParse,
+        error: errorOnAjax
+    });
+
+    $("#EPCRAoverlay").css({ display: "none" });
+});
+
+$("#CERCLAscrapeBtn").click(function (e) {
+    var inputXpath = $("#CERCLAtableXpath").val();
+    console.log("running CERCLA Scrapper with xpath: " + inputXpath);
+    $("#overlay").css({ display: "flex" });
+
+    $.ajax({
+        type: "POST",
+        async: false,
+        data: { xpath: inputXpath },
+        url: "/Admin/ScrapeCERCLAtable",
+        success: successParse,
+        error: errorOnAjax
+    });
+
+    $("#overlay").css({ display: "none" });
+});
+
+$("#UpdateChemsBtn").click(function (e) {
+    
+    console.log("attempting to update the chemical list via PubChem API calls");
+
+});
+
+function errorOnAjax() {
+    console.log("ERROR in ajax request");
+    $("#outputLog").val += ("ERROR in ajax request\n");
+    $("#overlay").css({ display: "none" });
+}
+
+function successParse(data) {
+    $("#outputLog").val += ("success\n");
+    //$("#overlay").css({ display: "none" });
+}
