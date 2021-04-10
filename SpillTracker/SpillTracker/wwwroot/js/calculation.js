b9057d890f410f76e1c009af1b280dd683fa1f63
NickApa <napa15@mail.wou.edu>
2021-04-10 12:47:56 -0700

    created the calculation page

---
+++ SpillTracker/SpillTracker/wwwroot/js/calculation.js
@@ -0,0 +1,37 @@
+console.log("js file works!")
+
+$("#calcButton").click(function() 
+// function calculate()
+{
+    let c = document.getElementById("chemName");
+    let chemical = c.textContent;
+    let p = document.getElementById("chemRW");
+    let text = p.textContent;
+    let reportableWeight = parseInt(text);
+
+    let amountReleased = $('#amountReleased').val();
+    let timeReleased = $('#timeReleased').val();
+    let percentReleased = $('#percentReleased').val()/100;
+
+    let totalRelease = (amountReleased * percentReleased).toFixed(2);
+    let releasePerHour = (totalRelease / timeReleased).toFixed(2);
+    let timeTillReport = (reportableWeight/releasePerHour) - timeReleased;
+    let dayRelease = (24*releasePerHour).toFixed(2);
+    
+    let tr = $("#totalReleased");
+    let rph = $("#releasePerHour")
+    let dr = $("#dayRelease")
+    $("#totalReleased").empty();
+    $("#releasePerHour").empty();
+    $("#dayRelease").empty();
+    tr.append("Amount released so far: " + totalRelease + " lbs")
+    rph.append("Amount released per hour: " + releasePerHour + " lbs")
+    dr.append("Assuming continuous release, over the course of 24 hours " + dayRelease + " lbs. will be released.")
+
+    if(dayRelease > reportableWeight) 
+    {
+        dr.append(" This exceeds the reportable quantity for " + chemical + ". You will need to report this to the proper authorities.")
+    }
+
+}
+)
