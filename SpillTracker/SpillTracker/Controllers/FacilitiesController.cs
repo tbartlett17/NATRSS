6f3a1f5a6c4889ea6fa437540f509b6beaac8171
Tyler Bartlett <tbartlett17@mail.wou.edu>
2021-04-10 12:58:20 -0700

    scaffolded facilities

---
+++ SpillTracker/SpillTracker/Controllers/FacilitiesController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SpillTracker.Models;
+
+namespace SpillTracker.Controllers
+{
+    public class FacilitiesController : Controller
+    {
+        private readonly SpillTrackerDbContext _context;
+
+        public FacilitiesController(SpillTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Facilities
+        public async Task<IActionResult> Index()
+        {
+            var spillTrackerDbContext = _context.Facilities.Include(f => f.Company).Take(0); // default show no facilities
+
+            if (User.IsInRole("admin")) // shows the admin all facilities
+            {
+                spillTrackerDbContext = _context.Facilities.Include(f => f.Company); // shows the admin all facilities
+            }
+            if (User.IsInRole("facility manager")) // shows the facilitiy manager only their facilities
+            {
+                // get the current users identity ID
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                var claim = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+                string userId = claim.Value;
+
+                // look up the current user in the spill tracker DB
+                Stuser thisUser = _context.Stusers.Where(stu => stu.AspnetIdentityId == userId).FirstOrDefault();
+                //Debug.WriteLine("\n\n STuser aspnet identity id: " + thisUser.AspnetIdentityId);
+
+                // select this user's company's facilities
+                spillTrackerDbContext = _context.Facilities.Where(f => f.CompanyId == thisUser.CompanyId).Include(f => f.Company); 
+            }
+
+            //var spillTrackerDbContext = _context.Facilities.Include(f => f.Company);
+            return View(await spillTrackerDbContext.ToListAsync());
+        }
+
+        // GET: Facilities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities
+                .Include(f => f.Company)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+
+            return View(facility);
+        }
+
+        // GET: Facilities/Create
+        public IActionResult Create()
+        {
+            ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Id");
+            return View();
+        }
+
+        // POST: Facilities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,AddressStreet,AddressCity,AddressState,AddressZip,Location,Industry,CompanyId")] Facility facility)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(facility);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Id", facility.CompanyId);
+            return View(facility);
+        }
+
+        // GET: Facilities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities.FindAsync(id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+            ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Id", facility.CompanyId);
+            return View(facility);
+        }
+
+        // POST: Facilities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,AddressStreet,AddressCity,AddressState,AddressZip,Location,Industry,CompanyId")] Facility facility)
+        {
+            if (id != facility.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(facility);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacilityExists(facility.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Id", facility.CompanyId);
+            return View(facility);
+        }
+
+        // GET: Facilities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities
+                .Include(f => f.Company)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+
+            return View(facility);
+        }
+
+        // POST: Facilities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var facility = await _context.Facilities.FindAsync(id);
+            _context.Facilities.Remove(facility);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacilityExists(int id)
+        {
+            return _context.Facilities.Any(e => e.Id == id);
+        }
+    }
+}
