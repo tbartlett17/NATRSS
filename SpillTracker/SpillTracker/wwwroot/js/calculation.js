92cdc16b3f6bf9b52ec2c6005ac96f5c0287341a
NickApa <napa15@mail.wou.edu>
2021-04-12 17:58:39 -0700

    added javascript testing and a few test for calculate

--- SpillTracker/SpillTracker/wwwroot/js/calculation.js
+++ SpillTracker/SpillTracker/wwwroot/js/calculation.js
@@ -1,7 +1,17 @@
 console.log("js file works!")
 
+function calculate(reportableWeight, amountReleased, timeReleased, percentReleased) {
+    let totalRelease = (amountReleased * percentReleased).toFixed(2);
+    let releasePerHour = (totalRelease / timeReleased).toFixed(2);
+    let timeTillReport = ((reportableWeight/releasePerHour) - timeReleased).toFixed(2);
+    console.log(timeTillReport);
+    let vals = [totalRelease, releasePerHour, timeTillReport];
+    return vals;
+    
+}
+
+
 $("#calcButton").click(function() 
-// function calculate()
 {
     let c = document.getElementById("chemName");
     let chemical = c.textContent;
@@ -34,40 +44,49 @@ $("#calcButton").click(function()
     $("#headergood").empty();
     $("#headerbad").empty();
      
-    
-    tr.append("Amount released so far: " + totalRelease + " lbs")
-    rph.append("Amount released per hour: " + releasePerHour + " lbs")
- 
-    
-    if(totalRelease > reportableWeight)
+    if(amountReleased == '' || timeReleased == '' || percentReleased == '') 
     {
-        document.getElementById("modal-content").style.backgroundColor = "tomato";
-        headerBad.append("Notice: The chemical spill needs to be reported to the proper authorities.")
-        if(cercla == "CERCLA" && cercla != null) 
-        {
-            dr.append(" Since this is a CERCLA chemical you will need to contact the National Response Center at 1-800-424-8802")
-        }
-
+        document.getElementById("modal-content").style.backgroundColor = "yellow";
+        headerBad.append("Please enter information into all of the input boxes!")
     }
-    else 
+    else
     {
-        document.getElementById("modal-content").style.backgroundColor = "springgreen";
-        headerGood.append("Notice: The chemical spill has not reached the reportable quantity")
-        ttr.append("At the current release rate of " + releasePerHour + " lbs/hr this spill will become reportable in ")
-        if(timeTillReport < 24 && timeTillReport > 1) 
+        tr.append("Amount released so far: " + totalRelease + " lbs")
+        rph.append("Amount released per hour: " + releasePerHour + " lbs")
+    
+        
+        if(totalRelease >= reportableWeight)
         {
-            ttr.append(timeTillReport + " hrs.");
+            document.getElementById("modal-content").style.backgroundColor = "tomato";
+            headerBad.append("Notice: The chemical spill needs to be reported to the proper authorities.")
+            if(cercla == "CERCLA" && cercla != null) 
+            {
+                dr.append(" Since this is a CERCLA chemical you will need to contact the National Response Center at 1-800-424-8802")
+            }
+
         }
-        else if(timeTillReport < 1)
+        else 
         {
-            timeTillReport = (timeTillReport * 60);
-            ttr.append(timeTillReport + " minutes")
-        }
-        else
-        {
-            timeTillReport = (timeTillReport / 24).toFixed(2);
-            ttr.append(timeTillReport + " days");
+            document.getElementById("modal-content").style.backgroundColor = "springgreen";
+            headerGood.append("Notice: The chemical spill has not reached the reportable quantity")
+            ttr.append("At the current release rate of " + releasePerHour + " lbs/hr this spill will become reportable in ")
+            if(timeTillReport < 24 && timeTillReport >= 1) 
+            {
+                ttr.append(timeTillReport + " hrs.");
+            }
+            else if(timeTillReport < 1)
+            {
+                timeTillReport = (timeTillReport * 60);
+                ttr.append(timeTillReport + " minutes")
+            }
+            else
+            {
+                timeTillReport = (timeTillReport / 24).toFixed(2);
+                ttr.append(timeTillReport + " days");
+            }
         }
     }
 }
 )
+
+export { calculate }
