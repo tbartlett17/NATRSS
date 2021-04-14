391a9fe0231e38275c3d0f43bb1a2dabe72e386b
Armani (Asus) <atufaga17@mail.wou.edu>
2021-04-13 21:30:42 -0800

    tests

---
+++ SpillTracker/SpillTracker/Utilities/BadEmailUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SpillTracker.Utilities
+{
+    public class BadEmailUtilities
+    {
+        public static double RegexDensityParse(string input)
+        {
+            double density = double.Parse(Regex.Match(input, @"^\d*\.*\d*").Value);
+            return density;
+        }
+    }
+}
---
+++ SpillTracker/SpillTrackerCS.Tests/UnitTests/BadEmailInput.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpillTrackerCS.Tests.UnitTests
+{
+    class BadEmailInput
+    {
+        [SetUp]
+        public void SetUp() 
+        {
+        }
+
+        [Test]
+        public void BadEmailInputRegister_test() 
+        {
+            //arrange
+            string input = "@gm@.com";
+            //act
+            string output = (model).name(input);
+            //assert
+            Assert.AreSame("a@g.com", output);
+
+        } 
+
+    }
+}
