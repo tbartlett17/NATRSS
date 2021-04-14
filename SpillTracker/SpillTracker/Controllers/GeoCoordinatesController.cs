fa1e59cf7ca9113fb86cbc0acb7a2a8e5f563099
Reggie Johnson WS <rljohns579@gmail.com>
2021-04-14 12:51:47 -0700

    fixed accessing json data errors

---
+++ SpillTracker/SpillTracker/Controllers/GeoCoordinatesController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpillTracker.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+namespace SpillTracker.Controllers
+{
+    public class GeoCoordinatesController : Controller
+    {
+
+        private readonly IConfiguration _config;
+
+
+
+        public GeoCoordinatesController(IConfiguration config)
+        {
+            _config = config;
+
+        }
+
+        // POST: GeoCoordinatesController/Create
+        [HttpPost] 
+        public IActionResult GetCoords(string streetAddress)
+        {
+
+            /*StreetAddress test = new StreetAddress { Street = "1120 18th st se", City = "Salem", State = "OR", PostalCode = "97302" };*/
+            StreetAddress newStreetAddress = JsonConvert.DeserializeObject<StreetAddress>(streetAddress);
+
+            string secret = _config["AJAX1:MapQuestAPIKey"];
+            string url = $"http://www.mapquestapi.com/geocoding/v1/address?key={secret}&street={newStreetAddress.Street}&city={newStreetAddress.City}&state={newStreetAddress.State}&postalCode={newStreetAddress.PostalCode}";
+            
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                string jsonString = null;
+                // TODO: You should handle exceptions here
+                using (WebResponse response = request.GetResponse())
+                {
+                    Stream stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream);
+                    jsonString = reader.ReadToEnd();
+                    reader.Close();
+                    stream.Close();
+                }
+
+                //The API call worked correctly now update the cid and molecular weight
+                Debug.WriteLine(jsonString);
+                JObject geo = JObject.Parse(jsonString);
+                GeoLocationCoordinates theCoordinates = new GeoLocationCoordinates();
+
+                theCoordinates.Longitude = (string)geo["results"][0]["locations"][0]["latLng"]["lng"];
+                theCoordinates.Latitude = (string)geo["results"][0]["locations"][0]["latLng"]["lat"];              
+                var jsonCoordinates = JsonConvert.SerializeObject(theCoordinates);
+                
+                return Json(jsonCoordinates);
+            }
+            catch
+            {
+                return StatusCode(500); ;
+            }
+        }
+
+    }
+}
