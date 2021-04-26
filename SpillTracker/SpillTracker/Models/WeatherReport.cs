49b145bd5200cc1b9a72d16c1c08aabb48ac028a
Tyler Bartlett <tbartlett17@mail.wou.edu>
2021-04-26 13:44:40 -0700

    first implementation of the owm api functionlity

---
+++ SpillTracker/SpillTracker/Models/WeatherReport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpillTracker.Models
+{
+    public class WeatherReport
+    {
+        public DateTime DateTime { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Temperature { get; set; }
+        public string TemperatureUnits { get; set; }
+        public double Humidity { get; set; }
+        public string HumidityUnits { get; set; }
+        public double WindSpeed { get; set; }
+        public string WindSpeedUnits { get; set; }
+        public string WindDirection { get; set; }
+        public string SkyConditions { get; set; }
+    }
+}
