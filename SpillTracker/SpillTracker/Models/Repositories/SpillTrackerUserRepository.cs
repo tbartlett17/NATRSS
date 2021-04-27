d6650d82a351f8b539242293b96e61043cf1bc04
Tyler Bartlett <tbartlett17@mail.wou.edu>
2021-04-26 18:15:32 -0700

    wrote simple interfaces for form and stuser

---
+++ SpillTracker/SpillTracker/Models/Repositories/SpillTrackerUserRepository.cs
@@ -0,0 +1,28 @@
+﻿using SpillTracker.Models;
+using SpillTracker.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpillTracker.Models.Repositories
+{
+    public class SpillTrackerUserRepository : Repository<Stuser>, ISpillTrackerUserRepository
+    {
+        public SpillTrackerUserRepository(SpillTrackerDbContext ctx) : base(ctx)
+        {
+
+        }
+
+        public virtual bool Exists(Stuser stu)
+        {
+            return _dbSet.Any(x => x.AspnetIdentityId == stu.AspnetIdentityId && x.FirstName == stu.FirstName && x.LastName == stu.LastName);
+        }
+
+        public virtual Stuser GetStuserByIdentityId(string identityID)
+        {
+            return _dbSet.Where(u => u.AspnetIdentityId == identityID).FirstOrDefault();
+        }
+
+    }
+}
