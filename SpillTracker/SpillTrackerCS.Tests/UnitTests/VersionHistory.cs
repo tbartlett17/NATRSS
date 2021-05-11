9e3ae5728912658afe5a6e8a2d7b27f6df413980
Armani (Asus) <atufaga17@mail.wou.edu>
2021-05-11 13:41:17 -0800

    added testing for version history

---
+++ SpillTracker/SpillTrackerCS.Tests/UnitTests/VersionHistory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SpillTracker.Utilities;
+using SpillTracker.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace SpillTrackerCS.Tests.UnitTests
+{
+    class VersionHistory
+    {
+        IConfiguration Configuration { get; set; }
+
+
+        [SetUp]
+        public void Setup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<VersionHistory>();
+
+            Configuration = builder.Build();
+        }
+
+        [Test]
+        public void VerifyDateIsNotNull()
+        {
+            //arrange
+            
+            var apiKey = Configuration["NatrGitkey"];
+
+            //act
+           
+            //assert
+           
+        }
+
+
+    }
+}
