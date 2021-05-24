497c74d6fe82d02895e96135da9d3578c0ab5486
Tyler Bartlett <tbartlett17@mail.wou.edu>
2021-05-24 14:26:18 -0700

    fixed form to show message to join a company for a user who does not have one instead of developer error page

--- SpillTracker/SpillTracker/Controllers/FormsController.cs
+++ SpillTracker/SpillTracker/Controllers/FormsController.cs
@@ -72,8 +72,11 @@ namespace SpillTracker.Controllers
                 //Stuser currentUser = _context.Stusers.Where(stu => stu.AspnetIdentityId == userId).FirstOrDefault();
 
 
+                if (stUser.CompanyId != null)
+                {
+                    formsList = _stfRepo.GetAllFormsByCompanyId((int)stUser.CompanyId);
+                }
 
-                formsList = _stfRepo.GetAllFormsByCompanyId((int)stUser.CompanyId);
 
                 Debug.WriteLine($"\nfac chem id: {formsList.FirstOrDefault().FacilityChemical.Id}\n");
                 //spillTrackerDbContext = _context.Forms.Include(f => f.Chemical)
