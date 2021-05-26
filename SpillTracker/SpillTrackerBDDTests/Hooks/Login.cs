152da118442d1926710d94e486c611a8e2144b88
NickApa <napa15@mail.wou.edu>
2021-05-26 11:48:43 -0700

    updated a few testing stuff and things

--- SpillTracker/SpillTrackerBDDTests/Hooks/Login.cs
+++ SpillTracker/SpillTrackerBDDTests/Hooks/Login.cs
@@ -12,9 +12,12 @@ namespace SpillTrackerBDDTests.Hooks
     class Login
     {
         private const string LoginUrl = "https://localhost:5001/Identity/Account/Login";
+        private const string FacilitiesUrl = "https://localhost:5001/Facilities";
         private IWebDriver _webDriver;
         private readonly string user = "tester7@example.com";
         private readonly string pass = "123456Aa*";
+        private readonly string user2 = "tester3@example.com";
+        private readonly string pass2 = "123456Aa*";
 
         private IWebElement LoginEmail => _webDriver.FindElement(By.Id("Input_Email"));
         private IWebElement LoginPass => _webDriver.FindElement(By.Id("Input_Password"));
